983c7409baf30b083a150f909333f1b3f2e0be77
unknown <harshika0102@gmail.com>
2023-07-23 06:41:28 +0530

    Added bootstrap navigation

---
+++ app/javascript/application.js
@@ -0,0 +1,10 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//= require rails-ujs
+//= require activestorage
+import { Turbo } from "@hotwired/turbo-rails";
+
+//= require_tree .
+
+
+import "bootstrap"
+import "controllers"
